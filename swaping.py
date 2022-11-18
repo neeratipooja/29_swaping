aaf04455396c81778d52dc5febdc455fe51b7763
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-18 22:36:25 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,12 @@
+'''
+DATE=17TH NOV 2022
+DAY= thursday
+TOPIC= Swapping
+AUTHOR= kiranmai
+'''
+list=[12,35,9,56,24]
+list[0],list[4]=list[4],list[0]
+print(list) #[24,35,9,56,12]
+list=[23,65,19,90] 
+list[0],list[2]=list[2],list[0]
+print(list)
